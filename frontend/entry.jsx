c3e01676781992ab7e49a8adb9a33fed314e7f41
Peter Lemiszki <plemiszki@gmail.com>
2022-06-14 20:40:33 -0400

    placeholder components

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,1 +1,25 @@
-console.log('in entry file');
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
+import Start from './components/start.jsx'
+import Schedule from './components/schedule.jsx'
+import Track from './components/track.jsx'
+
+window.addEventListener('DOMContentLoaded', () => {
+
+  const container = document.getElementById('container')
+  const root = createRoot(container)
+
+  switch (window.location.pathname) {
+    case '/':
+      root.render(<Start />)
+      break
+    case '/schedule':
+      root.render(<Schedule />)
+      break
+    case '/track':
+      root.render(<Track />)
+      break
+  }
+
+})
