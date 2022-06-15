000c24b265535d708324cc4b2fb8012fbc8ab3d9
Peter Lemiszki <plemiszki@gmail.com>
2022-06-14 20:25:00 -0400

    page routes

---
+++ app/controllers/public_controller.rb
@@ -0,0 +1,15 @@
+class PublicController < ApplicationController
+
+  def start
+    render 'container'
+  end
+
+  def schedule
+    render 'container'
+  end
+
+  def track
+    render 'container'
+  end
+
+end
