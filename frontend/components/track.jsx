fc3df4863b524b9056b80183426e661115be1a55
Peter Lemiszki <plemiszki@gmail.com>
2022-06-14 21:18:24 -0400

    finish tracking component

--- frontend/components/track.jsx
+++ frontend/components/track.jsx
@@ -7,6 +7,77 @@ export default function Track() {
   const [status, setStatus] = useState('')
   const [errorText, setErrorText] = useState('')
 
+  const submitForm = async (e) => {
+    e.preventDefault()
+    setStatus(null)
+
+    if (trackingNumber === '') {
+      setErrorText('Please enter a tracking number.')
+      return
+    }
+
+    setSentTrackingNumber(trackingNumber)
+
+    fetch(`/api/pickups/${trackingNumber}`)
+    .then(async (response) => {
+      if (response.status === 404) {
+        setErrorText(`Tracking number ${trackingNumber} not found.`)
+        return
+      }
+      if (!response.ok) {
+        setErrorText('There was an unknown error.')
+        return
+      }
+      let result = await response.json()
+      setStatus(result.status)
+      setErrorText(null)
+    })
+  }
+
+  const renderSuccessMessage = () => {
+    if (!status) {
+      return
+    }
+    const capitalizedStatus = status.charAt(0).toUpperCase() + status.slice(1)
+    return(
+      <>
+        <div className="success-message">
+          <p>Tracking Number: {sentTrackingNumber}</p>
+          <p>Status: {capitalizedStatus}</p>
+        </div>
+        <style jsx>{`
+          .success-message {
+            display: inline-block;
+            padding: 20px;
+            background: lightgreen;
+            margin: 20px 0;
+            border-radius: 6px;
+          }
+        `}</style>
+      </>
+    )
+  }
+
+  const renderErrorMessage = () => {
+    if (!errorText) {
+      return
+    }
+    return(
+      <>
+        <div className="error-message">{ errorText }</div>
+        <style jsx>{`
+          .error-message {
+            display: inline-block;
+            padding: 20px;
+            background: pink;
+            margin: 20px 0;
+            border-radius: 6px;
+          }
+        `}</style>
+      </>
+    )
+  }
+
   return(
     <>
       <div>
@@ -15,9 +86,11 @@ export default function Track() {
             <div className="logo"></div>
           </a>
           <h1>Track a Pickup</h1>
+          { renderSuccessMessage() }
+          { renderErrorMessage() }
           <p>Please enter your tracking number below.</p>
           <form onSubmit={ (e) => { submitForm(e) }}>
-            <input type="text" onChange={ (e) => setTrackingNumber(e.target.value) } value={ trackingNumber }></input>
+            <input type="text" onChange={ (e) => setTrackingNumber(e.target.value) } value={ trackingNumber } maxlength={ 20 }></input>
             <input type="submit" value="Track" />
           </form>
         </div>
